8169259d0460d7adf9cd9f09a033deba957a7d2d
henridupont <jnpontis@laposte.net>
2021-01-12 14:06:18 +0100

    Commit initial. Fichiers .gitignore et main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main():
+    casino = Casino(100000)
+    jeu1 = Roulette("my wheel", casino)
+    jeu2 = MachineASous("bandit manchot", casino)
+    joueur = Joueur("Clement", 1000)
+    joueur.entrer_casino(casino)
+    joueur.jouer()
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    a = 0
+    for i in range(0,10):
+        a += 2*i
+    print(a)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
